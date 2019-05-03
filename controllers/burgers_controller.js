9f69ddff9833d8429bc1f1d7a259e5c5b75e5885
Trevor <takettel@hotmail.com>
2019-05-03 18:23:58 -0500

    all functioning pieces

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,22 +4,25 @@ var router = express.Router();
 
 var burger = require("../models/burger.js");
 
+
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  burger.select(function(data) {
+  burger.all(
+    function(data) {
     var hbsObject = {
       burgers: data
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
-  });
+  }
+  );
 });
 
 router.post("/api/burgers", function(req, res) {
   burger.create([
     "burger_name", "devoured"
   ], [
-    req.body.burger_name, req.body.devoured
+    req.body.burger_name, false
   ], function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
@@ -32,7 +35,7 @@ router.put("/api/burgers/:id", function(req, res) {
   console.log("condition", condition);
 
   burger.update({
-    devoured: req.body.devoured
+    devoured: true
   }, condition, function(result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
--- models/burger.js
+++ models/burger.js
@@ -1,27 +1,27 @@
 let orm = require("../config/orm.js");
 
 let burger = {
-    select: function(cb) {
-        orm.selectAll(function(res) {
-            cb(res);
-        });
-    },
-    create: function(cols, vals, cb) {
-        orm.insertOne(cols, vals, function(res) {
-            console.log("This is the error: " + cb);
-            cb(res);
-        });
-    },
-    update: function(objColVals, condition, cb) {
-        orm.update(objColVals, condition, function(res) {
+    all: function(cb) {
+        orm.selectAll("burgers", function(res) {
           cb(res);
         });
-    },
-    delete: function(name, cb) {
-        orm.delete(name, function(res) {
-            cb(res);
+      },
+      // The variables cols and vals are arrays.
+      create: function(cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function(res) {
+          cb(res);
         });
-    }
-};
-
+      },
+      update: function(objColVals, condition, cb) {
+        orm.update("burgers", objColVals, condition, function(res) {
+          cb(res);
+        });
+      },
+      delete: function(condition, cb) {
+        orm.delete("burgers", condition, function(res) {
+          cb(res);
+        });
+      }
+    };
+    
 module.exports = burger;
